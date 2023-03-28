08b5882d83cccb650744f0103295e6aa295a4681
Nnaemeka <emekason128@gmail.com>
2023-03-28 11:37:28 +0100

    set up react router in app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
+import logo from './../images/logo.png';
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='bg-secondary px-9 py-6'>
+            <div className='flex items-center'>
+                <img src={logo} alt='logo' />
+                <h1 className='font-bold text-primary text-2xl ml-2'>E-Fashion</h1>
+            </div>
+        </div>
     );
 }
 
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,17 @@
-const ProductContext = () => {
-    return (
-        <div>ProductContext</div>
-    );
+import { createContext, useState, useEffect } from "react";
+
+export const ProductContext = createContext();
+
+const ProductContext = ({ children }) => {
+    // Product state
+    const [products, setProducts] = useState([])
+    //  Fetch products from fake store API
+    useEffect(() => {
+        const fetchData = async() => {
+            const response = await fetch();
+            const data = response.json()
+        }
+    })
 }
 
 export default ProductContext;
